331cce5d3c57d1b710858c2046c3072e536ff1cb
Josiah Morgan <123709411+Josiah-Morgan@users.noreply.github.com>
2024-06-10 12:53:51 -0400

    Create config.py

---
+++ resources/config.py
@@ -0,0 +1,23 @@
+
+
+audio_folder = "audio"
+video_folder = "video"
+image_folder = "image"
+finished_video_folder = "finished_videos"
+background_video_folder = "background_videos"
+
+# Reddit Details
+client_id = ""
+client_secret = ""
+username = ""
+password = ""
+user_agent = ""
+
+# Youtube Details
+refresh_token = ""
+access_token = ""
+
+
+# Subreddit you want to grab the post and comments from
+subreddit = "AskReddit"
+amount_of_comments = 5
